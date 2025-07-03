a6afc4b8850ba6231cd6aa703318314d1f31b517
chrislevyli <chris.levy@lendinvest.com>
2025-07-03 15:46:55 +0000

    Refactor jobs and metrics to utilize weekly_partition for improved scheduling and data aggregation

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/jobs.py
@@ -1,10 +1,12 @@
 import dagster as dg
+from dagster_essentials.defs.partitions import weekly_partition
 from dagster_essentials.defs.partitions import monthly_partition
 
 trips_by_week = dg.AssetSelection.assets("trips_by_week")
 
 weekly_update_job = dg.define_asset_job(
     name="weekly_update_job",
+    partitions_def=weekly_partition,
     selection=trips_by_week,
 )
 
